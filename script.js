a75b6df358a0623e2d369b34a3caa4cec9d5144e
student13 <student13@itpark2.local>
2023-09-29 10:13:22 +0500

    project_p8

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+  window.addEventListener('scroll', function () {
+    scrollY > 0 ? document.querySelector('.header').classList.add('scroll') : this.document.querySelector('.header').classList.remove('scroll');
+  });
+
+
+
